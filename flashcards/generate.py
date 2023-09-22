51e7e3e551b27d9e5e46216a371ca14b8fd20cdf
Feihong Hsu <feihong.hsu@gmail.com>
2023-09-21 23:30:31 -0500

    All cards for remaining glyphs

--- flashcards/generate.py
+++ flashcards/generate.py
@@ -7,6 +7,7 @@ prefix = """\
 #notetype column: 1
 #columns:notetype;front
 """
+note_type = 'bqn cloze'
 output_file = Path('output.txt')
 
 def get_rows(csvfile: Path):
@@ -19,16 +20,24 @@ def get_rows(csvfile: Path):
 
 with output_file.open('w') as fp:
   fp.write(prefix)
+
   for glyph, monadic, dyadic, input in get_rows('functions.csv'):
-    fp.write('bqn cloze;')
+    fp.write(f'{note_type};')
     fp.write('<p class="glyph">{{c1::%s}}</p> is monadic {{c2::%s}} and dyadic {{c3::%s}}.' % (glyph, monadic, dyadic))
     if input:
       fp.write(' Input it using {{c4::<code>\\%s</code>}}.' % input)
     fp.write('\n')
 
   for glyph, name, type_, input in get_rows('modifiers.csv'):
-    fp.write('bqn cloze;')
+    fp.write(f'{note_type};')
     fp.write('<p class="glyph">{{c1::%s}}</p> is {{c2::%s}} {{c3::%s}}.' % (glyph, type_, name))
     if input:
       fp.write(' Input it using {{c4::<code>\\%s</code>}}.' % input)
+    fp.write('\n')
+
+  for glyph, name, input in get_rows('other.csv'):
+    fp.write(f'{note_type};')
+    fp.write('<p class="glyph">{{c1::%s}}</p> is {{c2::%s}}.' % (glyph, name))
+    if input:
+      fp.write(' Input it using {{c3::<code>\\%s</code>}}.' % input)
     fp.write('\n')
